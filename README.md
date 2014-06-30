5aa1c23f03aa61f9de640a9f403691286160bf11
lemonsqueeze <lemonsqueeze@gmx.com>
2014-06-30 07:54:22 +0200

    working !

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 FakeWifiConnection
 ==================
 
-Make android apps believe they're on wifi.
+Make android apps believe Wifi is connected.
+
+Handy when using unofficial connection (usb 3g modem / ppp widget, usb reverse tethering ...) or on 3g connection and some app won't work unless it's on wifi.
+
+Tested on Android KitKat (4.4 RC2), play store working over ppp !
+
+You might want to try [HackConnectivity.apk](http://www.digitalmobile.in/community/threads/fake-wifi-to-play-games-with-wifi-requirement.8461/) as well. It didn't work for me, maybe it works with older android versions.
+
+Install
+-------
+
+- Install Xposed Framework.  
+  Get installer from http://repo.xposed.info/  
+  Open Xposed Installer->Framework->Install  
+  Reboot
+
+- Install [FakeWifiConnection.apk](https://raw.github.com/lemonsqueeze/FakeWifiConnection/master/bin/FakeWifiConnection.apk)  
+  Open Xposed Installer->Modules, tick FakeWifiConnection  
+  Reboot
+
+No easy way to enable/disable hack at this stage, just de/activate module in Xposed for now.
+
+Debugging
+---------
+
+For now only getActiveNetworkInfo() is faked. If app is calling other stuff like getNetworkInfo() or getAllNetworkInfo() you can find out with:
+
+`logcat | grep FakeWifiConnection`
+
+Also check Xposed logs:
+
+`logcat | grep Xposed`
---
+++ src/com/lemonsqueeze/fakewificonnection/Main.java
@@ -0,0 +1,98 @@
+package com.lemonsqueeze.fakewificonnection;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.XposedBridge;
+
+//import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+public class Main implements IXposedHookLoadPackage {
+	
+ public NetworkInfo	getFakeNetworkInfo() throws Exception
+ {
+	NetworkInfo info = createNetworkInfo(ConnectivityManager.TYPE_WIFI, true);
+	// XposedBridge.log("FakeWifiConnection: fakeNetworkInfo.isConnected(): " + info.isConnected());
+	return info;
+ }
+
+ public NetworkInfo createNetworkInfo(final int type, final boolean connected) throws Exception 
+ {
+	Constructor<NetworkInfo> ctor = NetworkInfo.class.getDeclaredConstructor(int.class);
+	ctor.setAccessible(true);
+	NetworkInfo networkInfo = ctor.newInstance(0);
+	Field typeField = NetworkInfo.class.getDeclaredField("mNetworkType");
+	Field connectedField = NetworkInfo.class.getDeclaredField("mState");
+	typeField.setAccessible(true);
+	connectedField.setAccessible(true);
+	typeField.setInt(networkInfo, type);
+	connectedField.set(networkInfo, connected == true ? NetworkInfo.State.CONNECTED : NetworkInfo.State.DISCONNECTED);
+	return networkInfo;
+ }
+ 
+ @Override
+ public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+	 // XposedBridge.log("FakeWifiConnection: Loaded app: " + lpparam.packageName);
+	 
+	//  Context context ...
+	//  ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	//  NetworkInfo info = cm.getActiveNetworkInfo();
+	//  "android.net.ConnectivityManager"
+	 
+	// targets:
+	// getActiveNetworkInfo()
+	// getNetworkInfo()
+	// getActiveNetworkInfoForUid()
+	// getAllNetworkInfo()
+		 
+	 
+	 // getActiveNetworkInfo()
+	 findAndHookMethod("android.net.ConnectivityManager", lpparam.classLoader, 
+			 "getActiveNetworkInfo", new XC_MethodHook() 
+	   {
+	     @Override
+         protected void beforeHookedMethod(MethodHookParam param) throws Throwable 
+         {
+	    	 XposedBridge.log("FakeWifiConnection: getActiveNetworkInfo(), faking wifi !");
+	    	 param.setResult(getFakeNetworkInfo());
+         }
+	   });
+
+	 // getAllNetworkInfo()
+	 findAndHookMethod("android.net.ConnectivityManager", lpparam.classLoader, 
+			 "getAllNetworkInfo", new XC_MethodHook() 
+	   {
+	     @Override
+         protected void beforeHookedMethod(MethodHookParam param) throws Throwable 
+         {
+	    	 XposedBridge.log("FakeWifiConnection: getAllNetworkInfo() called.");
+         }
+	   });
+
+	 
+	 // getNetworkInfo(int)
+	 findAndHookMethod("android.net.ConnectivityManager", lpparam.classLoader, 
+			 "getNetworkInfo", int.class, new XC_MethodHook() 
+	   {
+	     @Override
+	     protected void beforeHookedMethod(MethodHookParam param) throws Throwable 
+	     {	
+	    	 XposedBridge.log("FakeWifiConnection: getNetworkInfo(" + param.args[0] + ") called.");
+	     }
+	   });
+	 
+		 
+ }
+}
+
+
+
+
+
+
+
