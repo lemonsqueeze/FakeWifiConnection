62cb5a24f64a8f4e58b74a23be7ace182d5e7af7
lemonsqueeze <lemonsqueeze@gmx.com>
2014-08-23 20:13:57 +0200

    handle wifimanager getDhcpInfo(), also report correct IP address in getConnectionInfo(). bphone app now working !

--- FakeWifiConnection/src/com/lemonsqueeze/fakewificonnection/Main.java
+++ FakeWifiConnection/src/com/lemonsqueeze/fakewificonnection/Main.java
@@ -2,8 +2,6 @@ package com.lemonsqueeze.fakewificonnection;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.*;
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedBridge;
@@ -16,7 +14,14 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.SupplicantState;
+import android.net.DhcpInfo;
 import android.util.Log;
+
+import java.net.NetworkInterface;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.*;
+import org.apache.http.conn.util.InetAddressUtils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -42,6 +47,15 @@ public class Main implements IXposedHookLoadPackage
       Log.d("FakeWifiConnection", Log.getStackTraceString(new Exception()));
   }
 
+  public void log(String s)
+  {
+      if (debug_level < 1)
+	  return;
+      
+      //XposedBridge.log("FakeWifiConnection: " + s);
+      Log.d("FakeWifiConnection", s);
+  }
+    
   public void log_call(String s)
   {
       if (debug_level < 1)
@@ -122,15 +136,96 @@ public class Main implements IXposedHookLoadPackage
 //    private int mRssi;	/** Received Signal Strength Indicator */
 //    public static final String LINK_SPEED_UNITS = "Mbps";	/** Link speed in Mbps */
 //    private int mLinkSpeed;
-//    private InetAddress mIpAddress;
-//    private String mMacAddress;
-      
+
+      IPInfo ip = getIPInfo();            
       XposedHelpers.setIntField((Object)info, "mNetworkId", 1);      
       XposedHelpers.setObjectField((Object)info, "mWifiSsid", createWifiSsid());
       XposedHelpers.setObjectField((Object)info, "mSupplicantState", SupplicantState.ASSOCIATED);
-      XposedHelpers.setObjectField((Object)info, "mBSSID", "11:22:33:44:55:66");
+      XposedHelpers.setObjectField((Object)info, "mBSSID", "66:55:44:33:22:11");
+      XposedHelpers.setObjectField((Object)info, "mMacAddress", "11:22:33:44:55:66");
+      XposedHelpers.setObjectField((Object)info, "mIpAddress", ip.addr);
       
       return info;
+  }
+
+  public static class IPInfo
+  {
+      NetworkInterface intf;
+      InetAddress addr;
+      String ip;
+      int ip_hex;
+      int netmask_hex;
+  }
+    
+  // get current ip and netmask
+  public static IPInfo getIPInfo()
+	{
+	    try
+	    {
+		List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+		for (NetworkInterface intf : interfaces)
+		{
+		    List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+		    for (InetAddress addr : addrs)
+		    {
+			if (!addr.isLoopbackAddress())
+			{
+			    String sAddr = addr.getHostAddress().toUpperCase();
+			    boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+			    if (isIPv4)
+			    {
+				IPInfo info = new IPInfo();
+				info.addr = addr;
+				info.intf = intf;
+				info.ip = sAddr;
+				info.ip_hex = InetAddress_to_hex(addr);
+				info.netmask_hex = netmask_to_hex(intf.getInterfaceAddresses().get(0).getNetworkPrefixLength());
+				return info;
+			    }
+			}
+		    }
+		}
+	    } catch (Exception ex) { } // for now eat exceptions
+	    return null;
+	}
+
+  public static int netmask_to_hex(int netmask_slash)
+  {
+      int r = 0;
+      int b = 1;
+      for (int i = 0; i < netmask_slash;  i++, b = b << 1)
+	  r |= b;
+      return r;
+  }      
+
+  // for DhcpInfo 
+  private static int InetAddress_to_hex(InetAddress a)
+  {
+      int result = 0;
+      byte b[] = a.getAddress();		
+      for (int i = 0; i < 4; i++)
+	  result |= (b[i] & 0xff) << (8 * i);			  
+      return result;
+  }
+    
+      
+  public DhcpInfo createDhcpInfo() throws Exception
+  {      
+      DhcpInfo i = new DhcpInfo();
+      IPInfo ip = getIPInfo();
+      i.ipAddress = ip.ip_hex;
+      i.netmask = ip.netmask_hex;
+      i.dns1 = 0x04040404;
+      i.dns2 = 0x08080808;
+      // gateway, leaseDuration, serverAddress
+
+      String s = ("ip address: " + String.format("%x", i.ipAddress) +
+		  " netmask: /" + i.netmask +
+		  "dns1: " + String.format("%x", i.dns1) +
+		  "dns2: " + String.format("%x", i.dns2));
+      log(s);
+      
+      return i;
   }
     
   @Override
@@ -196,6 +291,7 @@ public class Main implements IXposedHookLoadPackage
       //   isWifiEnabled()      
       //   getWifiState()
       //   getConnectionInfo()
+      //   getDhcpInfo()
 
       // TODO do we need these:
       //   createWifiLock(string)
@@ -249,6 +345,21 @@ public class Main implements IXposedHookLoadPackage
 	      }
       });
 
+      // getDhcpInfo()
+      findAndHookMethod("android.net.wifi.WifiManager", lpparam.classLoader, 
+			"getDhcpInfo", new XC_MethodHook() 
+      {
+	  @Override
+	  protected void afterHookedMethod(MethodHookParam param) throws Throwable 
+	      {
+		  log_call("getDhcpInfo() called" +
+			   (hack_enabled() ? ", faking wifi !" : ""));
+		  
+		  if (hack_enabled())
+		      param.setResult(createDhcpInfo());
+	      }
+      });
+      
       // *************************************************************************************
       // debug only
       
--- TestWifi/src/com/lemonsqueeze/testwifi/MainActivity.java
+++ TestWifi/src/com/lemonsqueeze/testwifi/MainActivity.java
@@ -11,6 +11,10 @@ import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiManager.WifiLock;
 
+import java.net.NetworkInterface;
+import java.net.InetAddress;
+import org.apache.http.conn.util.InetAddressUtils;
+import java.util.*;
 
 public class MainActivity extends Activity
 {
@@ -25,6 +29,7 @@ public class MainActivity extends Activity
 		NetworkInfo net = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);		
 		String s = "Network Info (WIFI):";
 		s += "\nisConnected: " + net.isConnected();
+		s += "\nisAvailable: " + net.isAvailable();
 		s += "\ntype: " + net.getTypeName();
 		
 		net = cm.getActiveNetworkInfo();
@@ -51,9 +56,97 @@ public class MainActivity extends Activity
 		} catch(Exception e) {
 			s += "\n\nWifi lock: FAILED";
 		}
+
+		IPInfo info = getIPInfo();
+	    s += "\n\n ip address: " + info.ip + 
+		     String.format("  ( 0x%08x )", info.ip_hex);
+	    
+	    //byte b[] = info.addr.getAddress();
+		//s += ",  [" + String.format("%02x %02x %02x %02x] ) ", b[0], b[1], b[2], b[3]); 
+		
+		s += "\n netmask: " + String.format("0x%08x", info.netmask_hex);
 		
 		TextView text = new TextView(this);
 		text.setText(s);
         setContentView(text);
     }
+	
+
+	public static class IPInfo
+	{
+		NetworkInterface intf;
+		InetAddress addr;
+		String ip;
+		int ip_hex;
+		int netmask_hex;
+	}
+
+	// get current ip and netmask
+	public static IPInfo getIPInfo()
+	{
+		try
+		{
+			List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+			for (NetworkInterface intf : interfaces)
+			{
+				List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+				for (InetAddress addr : addrs)
+				{
+					if (!addr.isLoopbackAddress())
+					{
+						String sAddr = addr.getHostAddress().toUpperCase();
+						boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+						if (isIPv4)
+						{
+							IPInfo info = new IPInfo();
+							info.addr = addr;
+							info.intf = intf;
+							info.ip = sAddr;
+							info.ip_hex = InetAddress_to_hex(addr);
+							info.netmask_hex = netmask_to_hex(intf.getInterfaceAddresses().get(0).getNetworkPrefixLength());
+							return info;
+						}
+					}
+				}
+			}
+		} catch (Exception ex) { } // for now eat exceptions
+		return null;
+	}
+
+	public static int netmask_to_hex(int netmask_slash)
+	{
+		int r = 0;
+		int b = 1;
+		for (int i = 0; i < netmask_slash;  i++, b = b << 1)
+			r |= b;
+		return r;
+	}      
+
+	// for DhcpInfo 
+	private static int InetAddress_to_hex(InetAddress a)
+	{
+		int result = 0;
+		byte b[] = a.getAddress();		
+		for (int i = 0; i < 4; i++)
+			result |= (b[i] & 0xff) << (8 * i);			  
+//	  result |= (b[i] & 0xff) << (24 - (8 * i));		
+//	  result += b[i] << (24 - (8 * i));		
+		return result;
+	}
+	
+	// bad
+	/*
+	private static int InetAddress_to_hex(InetAddress a)
+	{
+		int result = 0;
+		byte b[] = a.getAddress();		
+		for (int i = 0; i < 4; i++) 
+    		result += b[i] << (24 - (8 * i));		
+		return result;
+	}
+	*/
+	
 }
+
+
+	
